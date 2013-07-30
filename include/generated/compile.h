042e6f178def699d4c82349d9c9d13ed0d221741
xens <xens0117@gmail.com>
2013-07-30 17:26:57 +0900

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 PREEMPT Tue Jul 30 17:14:15 KST 2013"
+#define LINUX_COMPILE_BY "MinL"
+#define LINUX_COMPILE_HOST "XENSATION"
+#define LINUX_COMPILER "gcc version 4.7.4 20130626 (prerelease) (Linaro GCC 4.7-2013.07-1~dev) "
